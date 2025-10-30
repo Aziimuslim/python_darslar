5c40ebc6baa47d4556df23600666a85ffe4af666
Aziimuslim <asqarovazizbek555@gmail.com>
2025-10-30 08:53:30 +0500

    16-dars

--- while/while.py
+++ while/while.py
@@ -2,3 +2,9 @@
 #30.10.2025
 #Asqarov Azizbek
 
+ism = input("Ismingiz nima?")
+print(f'Salom, {ism.title()}')
+
+ism = input("Ismingiz nima? ")
+savol = f"Salom, {ism.title()}. Yoshingiz nechida? "
+yosh = input(savol)
