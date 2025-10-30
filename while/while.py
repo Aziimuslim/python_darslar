4000d4a781c5a5c4e389b5a9778f12846f6712a4
Aziimuslim <asqarovazizbek555@gmail.com>
2025-10-30 16:13:17 +0500

    Lesson 17
    This lesson was about the WHILE tsikli. Python lessons continue. In the next lesson, we will perform operations on WHILE and dictionaries.

--- while/while.py
+++ while/while.py
@@ -100,3 +100,60 @@ while son>0:
         continue
     else:
         print(son) """
+
+#Amaliyot 
+""" savol ="Kiritilgan sonning ildizini qaytaruvchi dastur.\n"
+savol += "Musbat son kiriting "
+savol += "(dasturni to'xtatish uchun 'exit' deb yozing): "
+
+while True:
+    qiymat = input(savol)
+    if qiymat == 'exit':
+        break
+    else:
+        ildiz = float(qiymat)**(0.5)
+        print(f"{qiymat} ning ildizi {ildiz} ga teng") """
+
+""" savol = "Qaysi kitobni yaxshi korasiz: "
+savol +="(Tugagan bolsa, 'stop' deb yozing):"
+
+while True:
+    n =input(savol)
+    if n == 'stop':
+      break
+    else:
+       print("Yana bormi: ") """
+
+#Muzeyga chipta narhi foydalanuvchining yoshiga bog'liq: 7 dan yoshlarga - 2000 so'm, 7-18 gacha 3000 so'm, 18-65 gacha 10000 so'm, 65 dan kattalarga bepul. Shunday while tsikl yozingki, dastur foydalanuvchi yoshini so'rasin va chipta narhini chiqarsin. Foydalanuvchi exit yoki quit deb yozganda dastur to'xtasin (ikkita shartni ham tekshiring).
+
+while True:
+    yosh = input("Yoshingizni kiriting (chiqish uchun 'exit' yoki 'quit' deb yozing): ")
+
+    # chiqish sharti
+    if yosh.lower() in ['exit', 'quit']:
+        print("Dastur to‘xtatildi.")
+        break
+
+    # foydalanuvchi son kiritganini tekshiramiz
+    try:
+        yosh = int(yosh)
+    except ValueError:
+        print("Iltimos, butun son kiriting yoki 'exit'/'quit' deb yozing.")
+        continue
+
+    # chipta narxini aniqlash
+    if yosh < 7:
+        narx = 2000
+    elif yosh < 18:
+        narx = 3000
+    elif yosh < 65:
+        narx = 10000
+    else:
+        narx = 0
+
+    # natijani chiqarish
+    if narx == 0:
+        print("Sizga kirish bepul!")
+    else:
+        print(f"Sizning chipta narxingiz: {narx} so'm.")
+
