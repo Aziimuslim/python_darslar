8800ac5e40901efa3e03d3a4346ff2348c8adffa
Aziimuslim <asqarovazizbek555@gmail.com>
2025-11-03 10:49:05 +0500

    16 dars amaliy qisimgacha

--- while/while_lug'at.py
+++ while/while_lug'at.py
@@ -27,7 +27,7 @@ for ism in ismlar:
 '''
 #WHILE YORDAMIDA LUG'ATNI TO'LDIRISH
 
-print("Do'stingiz yoshini saqlaymiz. ")
+''''print("Do'stingiz yoshini saqlaymiz. ")
 dostlar = {}
 ishora = True
 while ishora:
@@ -41,4 +41,60 @@ while ishora:
 
 for ism, yosh in dostlar.items():
     print(f"{ism.title()} {yosh} yoshda")
+'''
+
+#RO'YXAT ELEMENTLARINI O'CHIRISH
+
+'''cars = ['lacetti', 'nexia', 'toyota', 'nexia', 'audi', 'malibu', 'nexia']
+while 'nexia' in cars:
+    cars.remove('nexia')
+print(cars) '''
+
+#RO'YXATDAN RO'YXATGA ELEMENT KO'CHIRISH
+'''talabalar = ['hasan','husan', 'olim', 'botir']
+baholangan_talabalar = {}
+while talabalar:
+    talaba = talabalar.pop()
+    baho =input(f"{talaba.title()}ning bahosini kiriting: ")
+    print(f"{talaba.title()} baholandi.")
+    baholangan_talabalar[talaba] = baho
+'''
+
+#Amaliyot
+#Foydalanuvchidan buyurtma qabul qiluvchi dastur yozing. Mahsulotlar nomini birma-bir qabul qilib, yangi ro'yxatga joylang.
+'''
+mahsulotar =[]
+print("Mahsulot sotib olish: ")
+n = 1
+while True:
+    savol =f"{n}-sotib oladigan mahsulot nomini kiriting: "
+    mahsulot = input(savol)
+    mahsulotar.append(mahsulot)
+    javob =  input("Yana mahsulot qo'shasizmi? (ha/yoq)")
+    if javob =='ha':
+        n+=1
+        continue
+    else:
+        break
+
+print("Mahsulotlar ro'yxati: ")
+for mahsulot in mahsulotar:
+    print(mahsulot.title())    '''
+'''
+print("e-bozor uchun mahsulotlar narxhini saqlaymiz: ")
+mahsulotar ={}
+ishora =True
+while ishora:
+    mahsulot = input("Mahsulot nomini kiriting: ")
+    narx = input(f"{mahsulot.title()}ning narxini kiriting: ")
+    mahsulotar[mahsulot]= int(narx)
+
+    javob = input("Yana mahsulot qo'shasizmi? (ha/yoq) ")
+    if javob == "yoq":
+        ishora = False
+
+for mahsulot, narx in mahsulotar.items():
+    print(f"{mahsulot.title()} {narx} yoshda")
+'''
+
 
