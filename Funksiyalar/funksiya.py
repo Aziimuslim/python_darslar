f780964c6dea5a11da82eb46658775a5216ddac8
Aziimuslim <asqarovazizbek555@gmail.com>
2025-11-04 10:32:48 +0500

    Update funksiya.py

--- Funksiyalar/funksiya.py
+++ Funksiyalar/funksiya.py
@@ -46,11 +46,47 @@ def toliq_ism(ism,familiya):
 
 toliq_ism('sardor', 'aliyev') '''
 
-
+'''
 def yosh_hisobla(ism, tugilgan_yil):
     """Foydalanuvchi yoshini hisoblaydigan dastur"""
     print(f"{ism.title()} {2025-tugilgan_yil} yoshda")
 #yosh_hisobla('sardor', 2005)    
 
 #KALIT SO'Z BILAN UZATISH
-yosh_hisobla(tugilgan_yil=2005, ism='sador')
+yosh_hisobla(tugilgan_yil=2005, ism='sador') '''
+
+#STANDART QIYMAT
+'''
+def yosh_hisobla(tugilgan_yil, joriy_yil=2025):
+    """Foydalanuvchi tug'ilgan yilidanuning yoshini hisoblaydi."""
+    print(f"Siz {joriy_yil-tugilgan_yil} yoshdasiz.")
+
+#yosh_hisobla(2000,2025)
+yosh_hisobla(2000) # faqat bitta argument bilan chaqirish
+'''
+#AMALIYOT
+#Foydalanuvchi ismi va yoshini so'rab, uning tug'ilgan yilini hisoblaydigan funksiya yozing.
+'''
+def ism_yosh(ism,yosh):
+    """Tug'ilgan yilini hisoblash"""
+    print(f"Siz {2025-yosh}-yilsiz.")
+
+ism_yosh('aziz',20) '''
+
+#Foydalanuvchidan son olib, uning kvadrati va kubini konsolga chiqaruvchi funksiya yozing.
+'''
+def kub_kv(son):
+    "Sonning kubi va kvadratini chiqarish"
+    print(f"Soning kubi: {son**2} va kvadrati: {son**3}")
+
+kub_kv(2)
+kub_kv(4) '''
+
+#Foydalanuvchidan son olib, son juft yoki toqligini konsolga chiqaruvchi funksiya yozing.
+def juft_toq(son):
+    """Juft va Toqlika tekshirish."""
+    print(f"Son juft {son%2==0}\n"
+          f"Son toq {son%2!=0}")
+    
+juft_toq(2)    
+juft_toq(5)
