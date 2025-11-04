b5210d68245d1bb93df1f3139f615739309cf260
Aziimuslim <asqarovazizbek555@gmail.com>
2025-11-04 09:40:13 +0500

    yangilladni

--- Funksiyalar/funksiya.py
+++ Funksiyalar/funksiya.py
@@ -18,6 +18,7 @@ def salom_ber(ism):
 
 salom_ber('aziz')   '''
 
+'''
 def salom_ber(ism):
     """Fodyalanuvchi ismini qabul qilib, 
         unga salom beruvchi funksiya"""   #docstring  deyiladi. funksiyaga malumot yozish.
@@ -26,3 +27,30 @@ def salom_ber(ism):
 salom_ber('aziz')
 
 print(salom_ber.__doc__) #docstring konsilga chiqarish. 
+'''
+
+#ARGUMENT VA PARAMETER
+'''Funksiya yaratishda, qavs ichida berilgan, funksiya to'g'ri ishlashi uchun uzatiladigan qiymat parameter deb ataladi. 
+   Yuqoridagi misolda ism bu salom_ber funksiyasining parametri.'''
+'''Foydalanuvchi funksiyaga murojat qilishda funksiyaga uzatgan qiymat esa argument deb ataladi.
+    salom_ber('hasan') kodida 'hasan' bu argument.'''
+
+#FUNKSIYAGA BIR NECHTA ARGUMENT UZATISH
+
+#TO'G'RI TARTIBDA UZATISH
+'''
+def toliq_ism(ism,familiya):
+    """Fodalanuvchi ism va familiyasini jamlab chiqaruvchi funksiya"""
+    print(f"Foydalanuvchi ismi: {ism.title()}\n"
+          f"Foydalanuvchi familiyasi: {familiya.title()}")
+
+toliq_ism('sardor', 'aliyev') '''
+
+
+def yosh_hisobla(ism, tugilgan_yil):
+    """Foydalanuvchi yoshini hisoblaydigan dastur"""
+    print(f"{ism.title()} {2025-tugilgan_yil} yoshda")
+#yosh_hisobla('sardor', 2005)    
+
+#KALIT SO'Z BILAN UZATISH
+yosh_hisobla(tugilgan_yil=2005, ism='sador')
